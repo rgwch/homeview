19a5e803c36a2eb94b9e7e7d15f19db94e3a6078
gerry <weirich@elexis.ch>
2017-09-14 17:25:20 +0200

    details app

--- src/allg.ts
+++ src/allg.ts
@@ -4,36 +4,54 @@ import {FetchClient} from './services/fetchclient'
 
 @autoinject
 export class Allg {
-  private buttons_def = {
+  private car_power=1104
+  private three_buttons_def = {
     buttons: [
       {
         caption: "An",
-        value  : 0
+        value  : 1
       }, {
         caption: "Aus",
-        value  : 1
+        value  : 0
       }, {
         caption: "Auto",
         value  : 2
       }
     ]
   }
-  private treppenlicht=Object.assign({message: "treppenlicht_state"}, this.buttons_def)
-  private tuerlicht=Object.assign({message: "tuerlicht_state"}, this.buttons_def)
-  private fernsehlicht=Object.assign({message: "fernsehlicht_state"}, this.buttons_def)
+  private two_buttons_def={
+    buttons:[
+      {
+        caption: "An",
+        value: 1
+      },{
+        caption: "Aus",
+        value: 0
+      }
+    ]
+  }
+  private treppenlicht=Object.assign({message: "treppenlicht_state"}, this.three_buttons_def)
+  private tuerlicht=Object.assign({message: "tuerlicht_state"}, this.three_buttons_def)
+  private fernsehlicht=Object.assign({message: "fernsehlicht_state"}, this.three_buttons_def)
+  private auto=Object.assign({message: "auto_state"}, this.three_buttons_def)
+  private mediacenter=Object.assign({message: "mediacenter_state"}, this.two_buttons_def)
+  private wlanext=Object.assign({message: "wlanextender_state"}, this.two_buttons_def)
+
 
 
   constructor(private ea:EventAggregator, fetcher:FetchClient){
     this.ea.subscribe(this.fernsehlicht.message+":click", event=>this.clicked(event,this.fernsehlicht))
     this.ea.subscribe(this.treppenlicht.message+":click", event=>this.clicked(event,this.treppenlicht))
     this.ea.subscribe(this.tuerlicht.message+":click", event=>this.clicked(event,this.tuerlicht))
-
+    this.ea.subscribe(this.mediacenter.message+":click",event=>this.clicked(event,this.mediacenter))
+    this.ea.subscribe(this.wlanext.message+":click",event=>this.clicked(event,this.wlanext))
+    this.ea.subscribe(this.auto.message+":click",event=>this.clicked(event,this.auto))
   }
 
   clicked(event,cfg){
-    if(event.value==0){
+    if(event.value==1){
       this.ea.publish(cfg.message,{state:"on"})
-    }else if(event.value==1){
+    }else if(event.value==0){
       this.ea.publish(cfg.message,{state: "off"})
     }
   }
